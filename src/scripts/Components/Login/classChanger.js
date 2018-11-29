784d543a89d6b3ca2375bf65f39a120cf83524c5
Max Milner <maxmilner31@yahoo.com>
2018-11-29 11:27:28 -0600

    figuring out hide/show

--- src/scripts/Components/Login/classChanger.js
+++ src/scripts/Components/Login/classChanger.js
@@ -1,1 +1,15 @@
-console.log("classChanger")
+function hideDiv() {
+  const login = document.getElementById("loginBox");
+  const messages = document.getElementById("messages");
+  const tasks = document.getElementById("taskBox");
+  const events = document.getElementById("eventBox");
+  const articles = document.getElementById("articleBox");
+
+  login.classList.toggle("hide") // Makes login & register disappear when pressed
+  messages.classList.toggle("hide") // Making messages appear after above
+  tasks.classList.toggle("hide")
+  events.classList.toggle("hide")
+  articles.classList.toggle("hide")
+}
+
+export default hideDiv
---
+++ src/scripts/main.js
@@ -0,0 +1,1 @@
+import hideDiv from "./Components/Login/classChanger"
