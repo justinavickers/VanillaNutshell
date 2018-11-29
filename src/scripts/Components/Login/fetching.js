1d3d88299f061071fd4dcf8dc49743c3b8600bbd
wesseln1 <nwessel429@gmail.com>
2018-11-29 11:07:06 -0600

    looking at pull request

--- src/scripts/Components/Login/fetching.js
+++ src/scripts/Components/Login/fetching.js
@@ -1,1 +1,25 @@
-console.log("Fetching login")
+const userAPIFunctions = {
+  //getting users from database to main.js
+  getUsers() {
+    return fetch("http://localhost:8088/users")
+      .then(users => users.json())
+      .then(parsedUsers => parsedUsers)
+  },
+  // fetch to validate email
+  getUser(email) {
+    return fetch(`http://localhost:8088/users?email=${email}`)
+      .then(user => user.json())
+      .then(parsedUser => parsedUser[0])
+  },
+  // fetch to piss new users into database
+  postUser(obj) {
+    console.log("http://localhost:8088/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(obj)
+    })
+  }
+}
+export default userAPIFunctions
