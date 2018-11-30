0b9d81e9d3f8a847a28e50ddc8b0760b959b2574
Daniel Combs <danielcombs@Daniels-MacBook-Pro.local>
2018-11-30 12:59:06 -0600

    making the password hide and working on displaying to dom

--- src/scripts/Components/Messages/fetching.js
+++ src/scripts/Components/Messages/fetching.js
@@ -1,1 +1,37 @@
-console.log("Fetching")
+const messageFetching={
+
+getFunction:() => {
+    fetch("http://localhost:8088/messages")
+    .then(messages => messages.json())
+    .then(parsedMessages => parsedMessages)
+},
+
+deleteFunction(){
+
+},
+
+postFunction(){
+        return fetch("http://localhost:8088/messages", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify(entry)
+
+        }).then(post => post.json())
+},
+
+putFunction(entry,id){
+        return fetch(`http://localhost:8088/messages/${id}`, {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify(entry)
+        }).then(res => res.json())
+        .then(console.log(entry))
+
+}
+}
+
+export default messageFetching
