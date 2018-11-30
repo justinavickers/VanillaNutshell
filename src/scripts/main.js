4124339a5105210068a5f5f6c076244ae22e4f69
wesseln1 <nwessel429@gmail.com>
2018-11-30 14:04:40 -0600

    removed unneeded code

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -18,20 +18,11 @@ existingUserButton.addEventListener("click", ()=>{
 taskNav.addEventListener("click", ()=> {
    tasksToDom()
 })
-
-
-messageComponent()
-
-
-// message.addEventListener("click", () => {
-   //    location.clear()
-   //    messageComponent.messageComponent()})
    // article.addEventListener("click", () =>{ hideDiv.hideArticleDiv()})
    // task.addEventListener("click", () => { hideDiv.hideTaskDiv()})
    // event.addEventListener("click", ()=> { hideDiv.hideEventDiv()})
    // event listener for sign up and register buttons
    // const message = document.querySelector("#messageNav")
    // const article = document.querySelector("#articleNav")
-   // const task= document.querySelector("#taskNav")
    // const event= document.querySelector("#eventNav")
 
