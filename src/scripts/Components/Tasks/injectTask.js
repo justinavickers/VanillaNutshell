93ec65a189901e3a12add2d4f28ae9f0c4d92b7c
wesseln1 <nwessel429@gmail.com>
2018-11-30 12:59:36 -0600

    fetching branch to look over for bugs

--- src/scripts/Components/Tasks/injectTask.js
+++ src/scripts/Components/Tasks/injectTask.js
@@ -1,29 +1,31 @@
 import taskAPIFunctions from "./taskFetching"
 import taskDiv from "./task"
-// import taskFormDiv from "./taskForm"
+
 let domTask = document.querySelector("#taskContainer")
-const taskInjection = function (parsedTasks) {
-    parsedTasks.forEach(task => {
-        let taskContents = taskDiv(task)
-        domTask.innerHTML = taskContents
-      })
-      console.log(parsedTasks, "parsed")
-    }
 
-    function clearTasks() {
-        domTask.innerHTML = ""
-      }
+// let domTask = document.querySelector("#taskContainer")
+// const taskInjection = function (parsedTasks) {
+//     parsedTasks.forEach(task => {
+//         let taskContents = taskDiv(task)
+//         domTask.innerHTML = taskContents
+//       })
+//       console.log(parsedTasks, "parsed")
+//     }
+
+//     function clearTasks() {
+//         domTask.innerHTML = ""
+//       }
 
 
-//     function injection(){
-//   innerHTML = ""
-//   taskFormDiv()
-// }
+// //     function injection(){
+// //   innerHTML = ""
+// //   taskFormDiv()
+// // }
 
-const  tasksToDom = () => {
-    clearTasks()
-    taskAPIFunctions.getTasks()
-    .then(parsedTasks =>
-      taskInjection(parsedTasks))
-    }
-export default tasksToDom
+// const  tasksToDom = () => {
+//     clearTasks()
+//     taskAPIFunctions.getTasks()
+//     .then(parsedTasks =>
+//       taskInjection(parsedTasks))
+//     }
+// export default tasksToDom
