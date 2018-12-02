2c0e3c303622d127a9146e0c2678b6f859ee99a3
Max Milner <maxmilner31@yahoo.com>
2018-12-02 14:21:57 -0600

    working on delete

--- src/scripts/Components/Events/eventDelete.js
+++ src/scripts/Components/Events/eventDelete.js
@@ -1,3 +1,11 @@
-function eventDelete() {
-  const event = document.getElementById("eventDiv").addEventListener("click", (e))
+import eventAPIfunctions from "./fetching"
+
+function deleteEvents(){
+eventAPIfunctions.getFunction().then(parsedEvent => parsedEvent.forEach(event =>{
+    if (event.id === document.querySelector(`".${event.id}"`)){
+        eventAPIfunctions.deleteFunction(`${event.id}`)
+    }
+}))
 }
+
+export default deleteEvents
