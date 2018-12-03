3c531478c7c5596137966aecb80ef33d466ce98f
Daniel Combs <danielcombs@Daniels-MacBook-Pro.local>
2018-12-03 08:52:00 -0600

    delete functionality works for messages now

--- src/scripts/Components/Messages/messageEventListners.js
+++ src/scripts/Components/Messages/messageEventListners.js
@@ -1,25 +1,28 @@
 import postMessage from "./postMessage"
 import deleteMessage from "./deleteMessage"
+// import editMessage from "./editMessage"
 const messageFunctionality= {
-editButton(){
-document.addEventListener("click",(e)=>{
-    if(e.target && e.target.id=== "editButton"){console.log("hello editButton")}}
-)
-},
+// editButton(){
+// document.addEventListener("click",(e)=>{
+//     if(e.target && e.target.id=== "editButton"){
+//         let Id = e.target.className
+//         let value = document.querySelector(".message").value
+//         editMessage(value, Id)
+//         console.log("hello editButton")}
+// })
+// },
 messageButton (){
 document.addEventListener("click", function (e){
     if (e.target && e.target.id=== "messageButton"){
         postMessage()
-        console.log("hello messageButton")}
+}
 })
 },
 deleteButton(){
 document.addEventListener("click", function (e){
     if (e.target && e.target.id=== "deleteButton"){
         let messageIdNumber =e.target.className
-        console.log(messageIdNumber)
-        deleteMessage(messageIdNumber)
-        console.log("hello deleteButton")}
+        deleteMessage(messageIdNumber)}
 })
 }
 }
